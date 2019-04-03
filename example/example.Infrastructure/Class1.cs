c01117036a5657bb0524be442387cf283b3acb36
kalitawojciech <wojtek1311997@gmail.com>
2019-04-03 10:51:07 +0200

    added StudentContext class

--- example/example.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace example.Infrastructure
-{
-    public class Class1
-    {
-    }
-}
---
+++ example/example.Infrastructure/StudentContext.cs
@@ -0,0 +1,21 @@
+﻿using example.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace example.Infrastructure
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options)
+            :base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
