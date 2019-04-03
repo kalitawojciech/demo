957013fa71697525281ecd9d31309218165df3e4
kalitawojciech <wojtek1311997@gmail.com>
2019-04-03 11:05:05 +0200

    added EFRepository

--- example/example.Infrastructure/EFRepository.cs
+++ example/example.Infrastructure/EFRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using example.Core.Entities;
+using example.Core.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace example.Infrastructure
 {
-    class EFRepository
+    class EFRepository<T> : IAsyncRepository<T> where T : BaseEntity
     {
+        protected readonly StudentContext _dbContext;
+        public EFRepository(StudentContext dbcontext)
+        {
+            _dbContext = dbcontext;
+        }
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
